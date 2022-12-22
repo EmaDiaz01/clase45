6d0277db67b3e7d548ff825274943fa7be1b4769
EmaDiaz01 <demanuel269@gmail.com>
2022-12-22 19:24:32 -0300

    salvando errores

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -9,7 +9,13 @@ export default function Navigation() {
                     <span className="navbar-toggler-icon"></span>
                 </button>
             </div>
-            
+            <div className="collapse navbar-collapse" id="navbarResponsive">
+                <ul className="nav ml-auto">
+                    <li className="nav-item">
+                        <button className="btn btn-success">Cargar random</button>
+                    </li>
+                </ul>
+            </div>
         </nav>
     )
 }
